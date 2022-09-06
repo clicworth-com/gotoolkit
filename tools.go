c78fecdf3cd1b26b1d71502b5705825bf102ac6b
cb-adhiman <87748037+cb-adhiman@users.noreply.github.com>
2022-09-06 13:05:37 +0530

    logs proto file added

--- tools.go
+++ tools.go
@@ -284,7 +284,6 @@ func (t *Tools) WriteJSON(w http.ResponseWriter, status int,data any, headers ..
 	return nil
 }
 
-
 func (t *Tools) ErrorJSON(w http.ResponseWriter, err error, status ...int) error {
 	statusCode := http.StatusBadRequest
 
