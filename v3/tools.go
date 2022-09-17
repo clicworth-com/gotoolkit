e5f7bb7b77d86289a4b2df2e5cbfcf90de3b23f5
cb-adhiman <87748037+cb-adhiman@users.noreply.github.com>
2022-09-17 13:45:41 +0530

    Update tools.go

--- v3/tools.go
+++ v3/tools.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -327,5 +328,35 @@ func (t *Tools) PushJSONToRemote(uri string, data interface{}, client ...*http.C
 
 	// send response back
 	return response, response.StatusCode, nil
+}
+
+func (t *Tools) GetIP(r *http.Request) (string, error) {
+    //Get IP from the X-REAL-IP header
+    ip := r.Header.Get("X-REAL-IP")
+    netIP := net.ParseIP(ip)
+    if netIP != nil {
+        return ip, nil
+    }
+
+    //Get IP from X-FORWARDED-FOR header
+    ips := r.Header.Get("X-FORWARDED-FOR")
+    splitIps := strings.Split(ips, ",")
+    for _, ip := range splitIps {
+        netIP := net.ParseIP(ip)
+        if netIP != nil {
+            return ip, nil
+        }
+    }
+
+    //Get IP from RemoteAddr
+    ip, _, err := net.SplitHostPort(r.RemoteAddr)
+    if err != nil {
+        return "", err
+    }
+    netIP = net.ParseIP(ip)
+    if netIP != nil {
+        return ip, nil
+    }
+    return "", fmt.Errorf("No valid ip found")
 }
 
