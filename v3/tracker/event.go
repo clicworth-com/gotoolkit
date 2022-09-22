aab0b319a76a526b8219ef1bab4695451fb4d8ea
cb-adhiman <87748037+cb-adhiman@users.noreply.github.com>
2022-09-22 19:57:24 +0530

    Update event.go

--- v3/tracker/event.go
+++ v3/tracker/event.go
@@ -11,27 +11,27 @@ var CWType string = "tracker.user.cw"
 var SearchType string = "tracker.user.search"
 
 type TrackerPayload struct {
-	Bid string `json:"bid"`
-	Type string `json:"type"`
-	Phone string `json:"phone"`
-	Name string `json:"name"`
-	Email string `json:"email"`
-	UtmSource string `json:"utm_source"`
-	UtmMedium string `json:"utm_medium"`
-	UtmCampaignId string `json:"utm_campaign_id"`
-	UtmCampaignName string `json:"utm_campaign_name"`
-	Lat float64 `json:"lat,omitempty"`
-	Lng float64 `json:"lng,omitempty"`
-	FloorNumber int32 `json:"floorNumber,omitempty"`
-	TotalPrice int32 `json:"totalPrice,omitempty"`
-	PricePerSqFt int32 `json:"pricePerSqFt,omitempty"`
-	AreaInSqft int32 `json:"areaInSqft,omitempty"`
-	Address string `json:"address"`
-	IpAddress string `json:"ipAddress"`
-	City string `json:"city"`
-	CbSearchListSize string `json:"cb_search_list_size"`
-	GoSearchListSize string `json:"go_search_list_size"`
-	TotalSearchListSize string `json:"total_search_list_size"`
+	Bid                 string  `json:"bid"`
+	Type                string  `json:"type"`
+	Phone               string  `json:"phone"`
+	Name                string  `json:"name"`
+	Email               string  `json:"email"`
+	UtmSource           string  `json:"utm_source"`
+	UtmMedium           string  `json:"utm_medium"`
+	UtmCampaignId       string  `json:"utm_campaign_id"`
+	UtmCampaignName     string  `json:"utm_campaign_name"`
+	Lat                 float64 `json:"lat,omitempty"`
+	Lng                 float64 `json:"lng,omitempty"`
+	FloorNumber         int32   `json:"floorNumber,omitempty"`
+	TotalPrice          int32   `json:"totalPrice,omitempty"`
+	PricePerSqFt        int32   `json:"pricePerSqFt,omitempty"`
+	AreaInSqft          int32   `json:"areaInSqft,omitempty"`
+	Address             string  `json:"address"`
+	IpAddress           string  `json:"ipAddress"`
+	City                string  `json:"city"`
+	CbSearchListSize    string  `json:"cb_search_list_size"`
+	GoSearchListSize    string  `json:"go_search_list_size"`
+	TotalSearchListSize string  `json:"total_search_list_size"`
 }
 
 type TrackerPublisher struct {
@@ -39,38 +39,44 @@ type TrackerPublisher struct {
 }
 
 type TrackerConsumer struct {
-	connection *amqp.Connection
-	client *mongo.Client
-	database *mongo.Database
+	connection       *amqp.Connection
+	client           *mongo.Client
+	database         *mongo.Database
 	searchCollection *mongo.Collection
-	cwCollection *mongo.Collection
+	cwCollection     *mongo.Collection
 }
 
 type TrackerEntry struct {
-	ID string `bson:"_id,omitempty" json:"id,omitempty"`
-	Bid string `bson:"bid" json:"bid"`
-	Type string `bson:"type" json:"type"`
-	Phone string `bson:"phone" json:"phone"`
-	Name string `bson:"name" json:"name"`
-	Email string `bson:"email" json:"email"`
-	UtmSource string `bson:"utm_source" json:"utm_source"`
-	UtmMedium string `bson:"utm_medium" json:"utm_medium"`
-	UtmCampaignId string `bson:"utm_campaign_id" json:"utm_campaign_id"`
-	UtmCampaignName string `bson:"utm_campaign_name" json:"utm_campaign_name"`
-	Lat float64 `bson:"lat,omitempty" json:"lat,omitempty"`
-	Lng float64 `bson:"lng,omitempty" json:"lng,omitempty"`
-	FloorNumber int32 `bson:"floorNumber,omitempty" json:"floorNumber,omitempty"`
-	TotalPrice int32 `bson:"totalPrice,omitempty" json:"totalPrice,omitempty"`
-	PricePerSqFt int32 `bson:"pricePerSqFt,omitempty" json:"pricePerSqFt,omitempty"`
-	AreaInSqft int32 `bson:"areaInSqft,omitempty" json:"areaInSqft,omitempty"`
-	Address string `bson:"address" json:"address"`
-	IpAddress string `bson:"ip_address" json:"ip_address"`
-	City string `bson:"city" json:"city"`
-	CbSearchListSize string `bson:"cb_search_list_size" json:"cb_search_list_size"`
-	GoSearchListSize string `bson:"go_search_list_size" json:"go_search_list_size"`
-	TotalSearchListSize string `bson:"total_search_list_size" json:"total_search_list_size"`
-	UpdatedAt int64 `bson:"updated_at" json:"updated_at"`
-	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+	ID                  string    `bson:"_id,omitempty" json:"id,omitempty"`
+	Bid                 string    `bson:"bid,omitempty" json:"bid,omitempty"`
+	Type                string    `bson:"type,omitempty" json:"type,omitempty"`
+	Phone               string    `bson:"phone,omitempty" json:"phone,omitempty"`
+	Name                string    `bson:"name,omitempty" json:"name,omitempty"`
+	Email               string    `bson:"email,omitempty" json:"email,omitempty"`
+	UtmSource           string    `bson:"utm_source,omitempty" json:"utm_source,omitempty"`
+	UtmMedium           string    `bson:"utm_medium,omitempty" json:"utm_medium,omitempty"`
+	UtmCampaignId       string    `bson:"utm_campaign_id,omitempty" json:"utm_campaign_id,omitempty"`
+	UtmCampaignName     string    `bson:"utm_campaign_name,omitempty" json:"utm_campaign_name,omitempty"`
+	Lat                 float64   `bson:"lat,omitempty" json:"lat,omitempty"`
+	Lng                 float64   `bson:"lng,omitempty" json:"lng,omitempty"`
+	FloorNumber         int32     `bson:"floorNumber,omitempty" json:"floorNumber,omitempty"`
+	TotalPrice          int32     `bson:"totalPrice,omitempty" json:"totalPrice,omitempty"`
+	PricePerSqFt        int32     `bson:"pricePerSqFt,omitempty" json:"pricePerSqFt,omitempty"`
+	AreaInSqft          int32     `bson:"areaInSqft,omitempty" json:"areaInSqft,omitempty"`
+	Address             string    `bson:"address,omitempty" json:"address,omitempty"`
+	IpAddress           string    `bson:"ip_address,omitempty" json:"ip_address,omitempty"`
+	City                string    `bson:"city,omitempty" json:"city,omitempty"`
+	CbSearchListSize    string    `bson:"cb_search_list_size,omitempty" json:"cb_search_list_size,omitempty"`
+	GoSearchListSize    string    `bson:"go_search_list_size,omitempty" json:"go_search_list_size,omitempty"`
+	TotalSearchListSize string    `bson:"total_search_list_size,omitempty" json:"total_search_list_size,omitempty"`
+	UpdatedAt           int64     `bson:"updated_at,omitempty" json:"updated_at,omitempty"`
+	CreatedAt           time.Time `bson:"created_at,omitempty" json:"created_at,omitempty"`
+	ConfidenceLevel     string    `bson:"confidenceLevel,omitempty" json:"confidenceLevel,omitempty"`
+	IsGoogleSearch      bool      `bson:"isGoogleSearch,omitempty" json:"isGoogleSearch,omitempty"`
+	CBProjectId         string    `bson:"cbprojectid,omitempty" json:"cbprojectid,omitempty"`
+	ClicworthPrice      int32     `bson:"clicworthPrice,omitempty" json:"clicworthPrice,omitempty"`
+	LowClicworthPrice   int32     `bson:"lowClicworthPrice,omitempty" json:"lowClicworthPrice,omitempty"`
+	HighClicworthPrice  int32     `bson:"highClicworthPrice,omitempty" json:"highClicworthPrice,omitempty"`
 }
 
 func setup(conn *amqp.Connection) error {
@@ -85,22 +91,22 @@ func setup(conn *amqp.Connection) error {
 func declareExchange(ch *amqp.Channel) error {
 	return ch.ExchangeDeclare(
 		"tracker_topic", //name
-		"topic", //type
-		true, //durable
-		false, //auto-delete
-		false, //internal
-		false, // no-wait
-		nil, //arguments
+		"topic",         //type
+		true,            //durable
+		false,           //auto-delete
+		false,           //internal
+		false,           // no-wait
+		nil,             //arguments
 	)
 }
 
 func declareRandomeQueue(ch *amqp.Channel) (amqp.Queue, error) {
 	return ch.QueueDeclare(
-		"", //name
+		"",    //name
 		false, //durable
 		false, //delete when unsued ?
-		true, // exclisive
+		true,  // exclisive
 		false, //no-wait?
-		nil, //arguments
+		nil,   //arguments
 	)
 }
